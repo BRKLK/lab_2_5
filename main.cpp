b49fe78074680f878e6fff24c287c69b0944089c
BRKLK <burkalyek@gmail.com>
2025-02-18 17:43:27 +0200

    done

--- main.cpp
+++ main.cpp
@@ -126,10 +126,92 @@ public:
         }
     }
 
-    int& front()
+    void insert(int index, int value)
     {
-        return arr[0];
+        len++;
+        int new_cap = nextPowerOfTwo(len);
+
+        if(new_cap == cap)
+        {
+            for(int i = len-1; i > index; i--) {arr[i] = arr[i-1];}
+
+            arr[index] = value;
+        }
+        else
+        {
+            int* new_arr = new int[new_cap];
+            cap = new_cap;
+
+            for(int i = 0; i < index; i++) {new_arr[i] = arr[i];}
+
+            new_arr[index] = value;
+
+            for(int i = index; i < len-1; i++) {new_arr[i+1] = arr[i];}
+
+            delete arr;
+            arr = new_arr;
+        }
     }
+
+    void erase(int pos)
+    {
+        len--;
+        int new_cap = nextPowerOfTwo(len);
+
+        if(new_cap == cap)
+        {
+            for(int i = pos; i < len+1; i++) {arr[i] = arr[i+1];}
+        }
+        else
+        {
+            int* new_arr = new int[new_cap];
+            cap = new_cap;
+
+            for(int i = 0; i < pos; i++) {new_arr[i] = arr[i];}
+
+            for(int i = pos+1; i < len+1; i++) {new_arr[i-1] = arr[i];}
+
+            delete arr;
+            arr = new_arr;
+        }
+    }
+
+    void erase(int first, int last)
+    {
+        int new_len = len - (last - first) - 1;
+        int new_cap = nextPowerOfTwo(new_len);
+
+        if(new_cap == cap)
+        {
+            for(int i = 0; (last + i + 1) < len; i++)
+            {
+                arr[first] = arr[last + i + 1];
+            }
+
+            len = new_len;
+        }
+        else
+        {
+            int* new_arr = new int[new_cap];
+            cap = new_cap;
+
+            for(int i = 0; i < first; i++) {new_arr[i] = arr[i];}
+
+            for(int i = last + 1; i < len; i++) {new_arr[i - 1 - (last - first)] = arr[i];}
+            // for(int i = first; )
+
+            len = new_len;
+            delete arr;
+            arr = new_arr;
+        }
+
+    }
+
+    int& front() {return arr[0];}
+    int& back() {return arr[len-1];}
+
+    bool empty() {return len == 0;}
+
 };
 
 
@@ -152,8 +234,16 @@ int main ()
     vec.push_back(1);
     vec.push_back(2);
     vec.push_back(3);
+    vec.push_back(4);
+    vec.push_back(5);
+    vec.push_back(6);
+    vec.push_back(7);
+    vec.push_back(8);
+    vec.push_back(9);
+    vec.push_back(10);
 
-    vec[0] = 5;
+
+    vec.erase(0, 6);
 
     for(int i = 0; i < vec.size(); i++)
     {
